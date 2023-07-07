0b5be876c977faaec902f5ccfb0292d6b89f6694
liucuxiu <xliucuxiux@gmail.com>
2023-07-07 20:32:24 +0700

    - add EventProcessor Cronjob

--- src/inventory/event/EventsProcessor.ts
+++ src/inventory/event/EventsProcessor.ts
@@ -1,6 +1,20 @@
-//create a cronjob
-// const cronJob = new CronJob('*/5 * * * * *', async () => {
-//     const inventory = await inventoryRepo.getInventory();
-//     inventoryRepo.updateInventory(inventory);
-// });
-// cronJob.start();
+import cron from 'node-cron';
+import { InventoryEventRepo } from './InventoryEventRepo';
+
+export class EventsProcessor {
+  private inventoryEventRepo: InventoryEventRepo;
+
+  constructor(inventoryEventRepo: InventoryEventRepo) {
+    this.inventoryEventRepo = inventoryEventRepo;
+    this.startCronJob();
+  }
+
+  private startCronJob(): void {
+    cron.schedule('*/10 * * * * *', async () => {
+      // Your task or function to be executed on schedule goes here
+      console.log('Cron job running every 5s');
+      const result = await this.inventoryEventRepo.getUnprocessedEvents();
+      console.log("cronjob result: ", result);
+    });
+  }
+}
